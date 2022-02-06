da20581b41c19d63a448dc0d6aaf7beb746e1928
gogotanaka <mail@tanakakazuki.com>
2022-02-06 13:12:30 +0900

    tweak

--- react-native/.eslintrc.js
+++ react-native/.eslintrc.js
@@ -9,39 +9,53 @@ module.exports = {
       version: 'detect',
     },
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'prettier'],
   plugins: ['react', 'react-hooks', 'react-native', '@typescript-eslint', 'simple-import-sort', 'prettier'],
   rules: {
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-use-before-define': 'error',
     'arrow-parens': ['error', 'as-needed'],
     'no-implicit-coercion': ['error', { boolean: false }],
     'object-shorthand': ['error', 'always'],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'react/prop-types': 'off',
-    'react-native/no-unused-styles': 'error',
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-module-boundary-types': 'off', // TODO
-    '@typescript-eslint/no-explicit-any': 'off', // TODO
-    '@typescript-eslint/explicit-function-return-type': 'off', // TODO
-    '@typescript-eslint/no-use-before-define': 'off', // TODO
-    '@typescript-eslint/camelcase': 'off', // TODO
-    '@typescript-eslint/ban-ts-comment': 'off', // TODO
-    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
     'prettier/prettier': [
       'error',
       {
         arrowParens: 'avoid',
-        jsxBracketSameLine: true,
+        bracketSameLine: true,
         singleQuote: true,
         trailingComma: 'all',
         printWidth: 120,
@@ -49,5 +63,11 @@ module.exports = {
         bracketSpacing: true,
       },
     ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'react/prop-types': 'off',
+    'react-native/no-unused-styles': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
 }
---
+++ react-native/README.md
@@ -0,0 +1,9 @@
+  $ yarn add -D eslint \
+@typescript-eslint/eslint-plugin \
+eslint-config-prettier \
+eslint-plugin-prettier \
+eslint-plugin-react \
+eslint-plugin-react-hooks \
+eslint-plugin-react-native \
+eslint-plugin-simple-import-sort \
+prettier
